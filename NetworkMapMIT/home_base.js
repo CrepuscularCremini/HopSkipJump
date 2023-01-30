a64e6a9e695f0481ad199864f666b415519b6812
RaunchyIguana <shelrock.shlome@protonmail.com>
2023-01-29 22:50:44 -0700

    mass ex

---
+++ NetworkMapMIT/home_base.js
@@ -0,0 +1,7 @@
+var home = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Point": "Origin" }, "geometry": { "type": "Point", "coordinates": [ -71.092945882625301, 42.359331186141745 ] } }
+]
+}
