84d517e4c81183f8663adfddd22809586cf7f6f2
RaunchyIguana <shelrock.shlome@protonmail.com>
2024-07-30 12:39:21 -0400

    create active transit map

--- brewsday.py
+++ brewsday.py
@@ -27,7 +27,7 @@ def brewsdaySource(df, odf, osm, gtfs, date, name, fp):
 
         transport_modes= [r5py.TransportMode.BICYCLE],
         speed_cycling = 18,
-        max_time = datetime.timedelta(minutes = 61),
+#         max_time = datetime.timedelta(minutes = 61),
         max_bicycle_traffic_stress = 2
     )
 
@@ -45,7 +45,7 @@ def brewsdaySource(df, odf, osm, gtfs, date, name, fp):
         percentiles = [20],
 
         transport_modes= [r5py.TransportMode.TRANSIT,r5py.TransportMode.WALK],
-        max_time = datetime.timedelta(minutes = 61)
+#         max_time = datetime.timedelta(minutes = 61)
     )
 
     transit_times = transit.compute_travel_times()
@@ -68,12 +68,12 @@ def brewsdayPick(fp, imped = 'all_imped', impval = 2, bike_weight = 1, transit_w
     if idx_exclude:
         gdf.query('id not in @idx_exclude', inplace = True)
 
-    gdf[['bike', 'transit']] = gdf[['bike', 'transit']].fillna(60)
+    gdf[['bike', 'travel_tim']] = gdf[['bike', 'travel_tim']].fillna(60)
 
     gdf.reset_index(inplace = True)
 
     gdf['bike_imped'] = (1 / gdf.bike) ** impval
-    gdf['transit_imped'] = (1 / gdf.transit) ** impval
+    gdf['transit_imped'] = (1 / gdf.travel_tim) ** impval
     gdf['all_imped'] = gdf.bike_imped * bike_weight + gdf.transit_imped * transit_weight
 
     imped = 'all_imped'
@@ -112,55 +112,58 @@ start = (-79.4079, 43.6566)
 
 odf = gpd.GeoDataFrame(pd.DataFrame.from_dict({'id' : [1], 'geometry' : [Point(start)]}), crs = 'EPSG:4326', geometry = 'geometry')
 
-fp = r"C:\Users\Brenn\Documents\Projects\HopSkipJump\Breweries\brewsday"
+fp = r"C:\Users\Brenn\Documents\Projects\HopSkipJump\Breweries\nrewsday"
 
 # brewsdaySource(df, odf, osm, gtfs, date, city, fp)
 
 gdf = gpd.read_file(fp)
 
 idx = '''
-501  - Halo - Mar 12
-487  - Mascot Brewery - Mar 19
-1075 - Collective Arts - Mar 26
-499  - Burdock - April 2
-     - Trinity Commons - April 9
-485  - Bellwoods Brewery - April 16
-515  - Left Field - April 23
-     - Blue Jays - April 30
-     - International - April 30
-486  - Goose Island - May 7
-1199 - Great Lakes Brewery - May 7
-1242 - Something in the Water - May 14
-488  - Bar Hop - May 21
-     - Blue Jays - May 21
-507  - Blood Brothers - May 26
-501  - Halo - May 26
-490  - Bandit - May 26
-484  - Northern Maverick - May 28
-1151 - True History Brewing - June 4
-497  - Saulter Street Brewery - June 11
-498  - Eastbound Brewing Company - June 11
-508  - High Park Brewery - June 18
-480  - Amsterdam Brewhouse - June 25
-     - International
+170 - Halo - Mar 12
+219 - Mascot Brewery - Mar 19
+104 - Collective Arts - Mar 26
+77  - Burdock - April 2
+    - Trinity Commons - April 9
+40  - Bellwoods Brewery - April 16
+201 - Left Field - April 23
+200 - (Left Field Other Location)
+    - Blue Jays - April 30
+    - International - April 30
+163 - Goose Island - May 7
+159 - Great Lakes Brewery - May 7
+314 - Something in the Water - May 14
+    - Bar Hop - May 21
+    - Blue Jays - May 21
+56  - Blood Brothers - May 25
+170 - Halo - May 25
+25  - Bandit - May 25
+248 - Northern Maverick - May 28
+356 - True History Brewing - June 4
+295 - Saulter Street Brewery - June 11
+129 - Eastbound Brewing Company - June 11
+173 - High Park Brewery - June 18
+15  - Amsterdam Brewhouse - June 25
+    - International
+48  - Big Rock Liberty Commons - July 9
+201 - Left Field - July 9
+172 - Henderson Brewing - July 16
+    - Granite Brewing - July 20
+    - Blue Jays - July 23
 '''
 
 idx = [x.split('-')[0].strip() for x in idx.split('\n')]
 idx = [int(x) for x in idx if x != '']
 
 ab = '''
-482  - Belgian Moon
-483  - Steam Whistle
+37 - Belgian Moon
+325 - Steam Whistle
 '''
 ab = [x.split('-')[0].strip() for x in ab.split('\n')]
 ab = [int(x) for x in ab if x != '']
 
 iss = '''
-496  - Kensington Brewery - not open yet
-509  - Ace Hill -
-1167 - Steadfast - not open on Tuesdays
-516  - Louis Cifer - perm closed
-481  - Liberty Commons - too restauranty imo
+324 - Steadfast - not open on Tuesdays
+212  - Louis Cifer - perm closed
 '''
 iss = [x.split('-')[0].strip() for x in iss.split('\n')]
 iss = [int(x) for x in iss if x != '']
